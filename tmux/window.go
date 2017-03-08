386d173737d867257a5e8ea0aceed0b9ded66b29
Nelo Wallus <nelo@wallus.de>
2017-03-08 07:27:25 +0100

    Replace exec.Command(...).Run() pattern with function

--- tmux/window.go
+++ tmux/window.go
@@ -1,7 +1,5 @@
 package tmux
 
-import "os/exec"
-
 // Function window creates a new window with the title s.
 func NewWindow(s string) error {
 	cmds := []string{"new-window"}
@@ -9,15 +7,12 @@ func NewWindow(s string) error {
 		cmds = append(cmds, "-n", s)
 	}
 
-	c := exec.Command("tmux", cmds...)
-	return c.Run()
+	return cmd(args)
 }
 
 // Function split splits the current pane and applies the passed layout
 func SplitWindow(layout string) error {
-	cmds := []string{"split-window"}
-	c := exec.Command("tmux", cmds...)
-	err := c.Run()
+	err := cmd([]string{"split-window"})
 	if err != nil {
 		return err
 	}
@@ -27,7 +22,5 @@ func SplitWindow(layout string) error {
 
 // Function SelectLayout applies the passed layout to the current window
 func SelectLayout(layout string) error {
-	cmds := []string{"select-layout", layout}
-	c := exec.Command("tmux", cmds...)
-	return c.Run()
+	return cmd([]string{"select-layout", layout})
 }
