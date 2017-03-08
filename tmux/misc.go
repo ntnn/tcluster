58280128d0c0c35d258782435e57567badb6af4d
Nelo Wallus <nelo@wallus.de>
2017-03-08 07:31:04 +0100

    Remove unpacking of slice

--- tmux/misc.go
+++ tmux/misc.go
@@ -13,5 +13,5 @@ func cmd(cmds []string) error {
 // Function SendKeys executes tmux send-keys and ends with a newline
 func SendKeys(s string) error {
 	args := fmt.Sprintf("send-keys %s\n", s)
-	return cmd(strings.Split(args, " ")...)
+	return cmd(strings.Split(args, " "))
 }
