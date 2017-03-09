5cbd3e1762ab03f5c7e69777a396320fc7c9b854
Nelo Wallus <nelo@wallus.de>
2017-03-09 17:57:25 +0100

    Remove openHosts, handled in cmd/ssh

--- tcluster.go
+++ tcluster.go
@@ -5,26 +5,7 @@ import (
 	"os"
 
 	"github.com/ntnn/tcluster/cmd"
-
-	"github.com/spf13/cobra/cobra/cmd"
 )
-
-func openHosts(title string, hosts []string) error {
-	if len(hosts) == 0 {
-		return ErrEmptyHostList
-	}
-
-	window(title)
-
-	for i := range hosts {
-		ssh(hosts[i])
-		if i < len(hosts)-1 {
-			split(conf.Layouts[conf.Layout])
-		}
-	}
-
-	return nil
-}
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
